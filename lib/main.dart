750419f4e6d65f17d35634c7e4b9f32a04d5c410
Muhamad Fathul Azis <azisfathl@gmail.com>
2023-12-03 14:14:48 +0700

    Implement Navigator 2.0 Using Page API

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:declarative_navigation/screen/quote_detail_screen.dart';
 import 'package:declarative_navigation/screen/quotes_list_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -7,15 +8,53 @@ void main() {
   runApp(const QuotesApp());
 }
 
-class QuotesApp extends StatelessWidget {
+class QuotesApp extends StatefulWidget {
   const QuotesApp({Key? key}) : super(key: key);
+
+  @override
+  State<QuotesApp> createState() => _QuotesAppState();
+}
+
+class _QuotesAppState extends State<QuotesApp> {
+  String? selectedQuote;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Quotes App',
-      home: QuotesListScreen(
-        quotes: quotes,
+      // home: QuotesListScreen(quotes: quotes),
+      home: Navigator(
+        pages: [
+          MaterialPage(
+            key: const ValueKey("QuotesListPage"),
+            child: QuotesListScreen(
+              quotes: quotes,
+              onTapped: (String quoteId) {
+                setState(() {
+                  selectedQuote = quoteId;
+                });
+              },
+            ),
+          ),
+          if (selectedQuote != null)
+            MaterialPage(
+              key: ValueKey("QuoteDetailsPage-$selectedQuote"),
+              child: QuoteDetailsScreen(quoteId: selectedQuote!),
+            ),
+        ],
+        onPopPage: (route, result) {
+          final didPop = route.didPop(result);
+
+          if (!didPop) {
+            return false;
+          }
+
+          setState(() {
+            selectedQuote = null;
+          });
+
+          return true;
+        },
       ),
     );
   }
--- lib/screen/quote_detail_screen.dart
+++ lib/screen/quote_detail_screen.dart
@@ -13,17 +13,16 @@ class QuoteDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final quote = quotes.singleWhere((element) => element.id == quoteId);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(quote.author),
-      ),
+      appBar: AppBar(title: Text(quote.author)),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(quote.author, style: Theme.of(context).textTheme.headline6),
-            Text(quote.quote, style: Theme.of(context).textTheme.subtitle1),
+            Text(quote.author, style: Theme.of(context).textTheme.titleLarge),
+            Text(quote.quote, style: Theme.of(context).textTheme.titleMedium),
           ],
         ),
       ),
